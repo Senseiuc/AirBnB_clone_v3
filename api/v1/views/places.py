14c6c89442fd17f5ccc0d63efa5d8ed9f47d6b5b
senseiuc <dhinhojboy@gmail.com>
2023-05-01 05:58:23 +0000

    place search

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -84,3 +84,43 @@ def update_place(place_id):
             setattr(place, attr, val)
     place.save()
     return jsonify(place.to_dict())
+
+
+@app_views.route('/places_search', methods=['POST'], strict_slashes=False)
+def post_places_search():
+    """searches place"""
+    if request.get_json() is not None:
+        vals = request.get_json()
+        states = vals.get('states', [])
+        cities = vals.get('cities', [])
+        amenities = vals.get('amenities', [])
+        amenity_objects = []
+        for amenity_id in amenities:
+            amenity = s.get('Amenity', amenity_id)
+            if amenity:
+                amenity_objects.append(amenity)
+        if states == cities == []:
+            places = s.all('Place').values()
+        else:
+            places = []
+            for state_id in states:
+                state = s.get('State', state_id)
+                state_cities = state.cities
+                for city in state_cities:
+                    if city.id not in cities:
+                        cities.append(city.id)
+            for city_id in cities:
+                city = s.get('City', city_id)
+                for place in city.places:
+                    places.append(place)
+        conf_places = []
+        for place in places:
+            place_amenities = place.amenities
+            conf_places.append(place.to_dict())
+            for amenity in amenity_objects:
+                if amenity not in place_amenities:
+                    conf_places.pop()
+                    break
+        return jsonify(conf_places)
+    else:
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)

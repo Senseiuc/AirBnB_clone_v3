af1f9284d46c517c3eaa19c3cbcad9aab934be5f
malik <mlanlokun@gmail.com>
2023-05-02 21:48:44 +0300

    pep8 standards

--- api/v1/views/0-main.py
+++ api/v1/views/0-main.py
@@ -13,17 +13,6 @@ if __name__ == "__main__":
 
     """ PUT /api/v1/users/<user_id>
     """
-    r = requests.put("http://0.0.0.0:5000/api/v1/users/{}".format(user_id), data=json.dumps({ 'first_name': "newfirstname" }), headers={ 'Content-Type': "application/json" })
+    r = requests.put("http://0.0.0.0:5000/api/v1/users/{}".format(user_id), data={ 'first_name': "NewFirstName" }, headers={ 'Content-Type': "application/json" })
     print(r.status_code)
-    r_j = r.json()
-    print(r_j.get('id') == user_id)
-    print(r_j.get('first_name') == "newfirstname")
-
-    """ Verify if the state is updated
-    """
-    r = requests.get("http://0.0.0.0:5000/api/v1/users")
-    r_j = r.json()
-    for user_j in r_j:
-        if user_j.get('id') == user_id:
-            print(user_j.get('first_name') == "newfirstname")
-        
+    print(r.content)    
